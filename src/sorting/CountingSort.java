1533c364438290f65815d8ab1d3dec4e205d231a
ZEENAT AGWAN <ZEENAT AGWAN@LAPTOP-TR2U9NMD>
2020-09-22 19:05:18 +0530

    Using Java

---
+++ src/sorting/CountingSort.java
@@ -0,0 +1,68 @@
+package sorting;
+//doesn't make comparisons for sorting.
+//to sort keys(n) we have to assume a range(k).
+import java.util.Arrays;
+
+//consider an temp array and create blocks according to the range to check  frequency of keys.Move from left to right and counts the occurance of keys.
+//write down the keys according to there occurrences but in sequential manner.ex:1 1 2 2 3 3 3 6 6 9
+//O(n+k) where k should be O(n).It cant be O(n2).that cause of unnecessary occurances of Zeros.
+//ex: 1 2 400 5 6 where k=400 (wastage of resource)
+//it cant be applied if the values are different in any range.(for this Radix sort)
+//keys cant be less than 1 and greater than range(max).It should be within its range.
+//this algo cant works directly for negative ,string or floating point values.
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] input = {2,5,9,8,2,7,10,4,8};
+      //  countingSort(input ,min: 1,max: 2);
+        countingSort(input, 1,10);
+        Arrays.stream(input).forEach(x->System.out.print(x + " "));
+    }
+    private static void countingSort(int[] input, int min ,int max){
+         int[] countingArray=new int [(max-min)+1];
+    for(int i=0;i<input.length;i++){
+      int no=input[i];
+      countingArray[no-1]=countingArray[no-1]+1;
+         }
+    
+    int counter=0;
+    for(int i=0;i<countingArray.length;i++){
+    int no=countingArray[i];
+    for(int j=0;j<no;j++){
+        input [counter]=i+1;
+        counter++;
+    }
+}
+}
+}
+//  static int range = 0;
+//    public static void countingSort(int[] array) {
+//      
+//
+//        int[] values = new int[range + 1];
+//        for (int i = 0; i <= range; i++) {
+//            values[i] = 0;
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            values[array[i]]++;
+//        }
+//        int[] arr = new int[array.length];
+//        int k = 0;
+//        for (int i = 0; i <= range; i++) {
+//            for (int j = 0; j < values[i]; j++) {
+//                arr[k++] = i;
+//            }
+//        }
+//        array = arr;
+//    }
+//
+//    public static void main(String args[]) {
+//        range = 9;
+//        int[] a = new int[]{1, 5, 0, 6, 9, 5, 0, 3, 2, 4, 5, 8, 7, 6, 1, 4};
+//        countingSort(a);
+//        for (int i = 0; i < a.length; i++) {
+//            System.out.print(a[i] + " ");
+//        }
+//    }
+
+
--- src/sorting/HeapSort.java
+++ src/sorting/HeapSort.java
@@ -1,50 +1,72 @@
+package sorting;
+/*
+to get child  index position:
+2*i+1=left child index
+2*i+2=right child index
 
-public class HeapSort {
+to get parent index position:
+i-1/2  //where i is index position
+*/
+public class HeapSort{
+public static void  main(String args[]){
+int arr[]={44,20,26,12,10,40,65};
+HeapSort hs=new HeapSort();
+hs.sort(arr);
+hs.printArray(arr);
+}
+//STEP 1: child(left/right) nodes's value should not be greater than its parent node.
+void sort(int arr[]){
+    
+int length =arr.length;
+for(int i =length/2 -1;i>=0;i--){    //len/2-1 which is an index position of right subtree's parent node.
+heapify(arr,length,i);                   //here i is index position of parent node.
 
-    public static void main(String[] args) {
-        int arr[] = {22, 13, 17, 11, 10, 14, 12};
-        HeapSort hs = new HeapSort();
-        hs.sort(arr);
-        hs.printArray(arr);
-    }
+}
+//STEP 2: Swap the elements and heapify again
+for(int i=length-1;i>=0;i--)        //because this time we exclude the last element and swap the new array's element (last becomes first and last becomes first)
+{
+int temp=arr[0];
+arr[0]=arr[i];
+arr[i]=temp;
+heapify(arr,i ,0);
+}
 
-    void sort(int arr[]) {
-        int size = arr.length;
-        for (int i = size / 2 - 1; i >= 0; i--) {
-            heapify(arr, size, i);
-        }
-        //(for rest of )swap the elements and heapify again
-        for (int i = size - 1; i >= 0; i--) {  //size -1 bcz one element is already at its right place
-            int temp = arr[0];  //swap 
-            arr[0] = arr[i]; //largest
-            arr[i] = temp;
-            heapify(arr, i, 0);//for every heapify it starts with index 0
-        }
-    }
+}
+void heapify(int [] arr ,int n,int i) {                                 //arr ,len,parent
+    
+//to find index position of child nodes
 
-    void heapify(int arr[], int n, int i) {
-        int largest = i;      //parent  node index position
-        int li = 2 * i + 1;      //left child node index position
-        int ri = 2 * i + 2;      //right child node index position
+int largest=i;                      //since i is index pos of parent node
+int left=2*i+1;                   // index position of left child
+int right=2*i+2;                //index position of right child
 
-        if (li < n && arr[li] > arr[largest]) {
-            largest = li; //swap
-        }
-        if (ri < n && arr[ri] > arr[largest]) {
-            largest = ri; //swap
-        }
-        if (largest != i) {
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
-            heapify(arr, n, largest);//this time we are passing largest
-        }
+//if the value of left child is greater than largest 
+if(left<n && arr[left] > arr[largest]){
+largest=left;
+}
 
-    }
+//if the value of right child is greater than largest 
+if(right <n && arr [right]>arr[largest]){
+largest=right;
+}
 
-    void printArray(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
-        }
-    }
+//if parent node's index position is not equal to largest then swap them.
+if(largest !=i){
+int temp=arr[i];
+arr[i]=arr[largest];
+arr[largest]=temp;
+
+heapify(arr , n ,largest);  
+}
+}
+
+//PRINT THE ARRAY
+void printArray(int[] arr) {
+for(int i=0;i<arr.length;i++) {
+System.out.println(arr[i]+ " " );
+}
+}
+
+
+
 }
